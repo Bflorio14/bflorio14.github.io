f9ae4ac96de024342931dab7b27da75b29317d4e
Brianna Florio <brianna.florio@stonybrook.edu>
2019-09-12 14:06:00 -0400

    reset stage 1

--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-function updateProgressBar() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("progress-bar").style.width = scrolled + "%";
-}
-
-window.onscroll = function() { updateProgressBar() };
